29be5a1b3df680042e890b61c894a508414a4bdb
Chaostastic <roodey72mc@gmail.com>
2022-11-04 13:03:09 +0200

    Generate grid through js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const gridSize = 16;
+const grid = document.querySelector(".grid");
+
+function generateGrid() {
+    for (let rowIndex = 1; rowIndex <= gridSize; rowIndex++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        grid.appendChild(gridRow);
+        for (let pixelIndex = 1; pixelIndex <= gridSize; ++pixelIndex) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            gridRow.appendChild(pixel);
+        }
+    }
+}
+
+generateGrid();
+
 const pixels = document.querySelectorAll(".pixel");
-pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel))
+pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel));
 
 function colorPixel() {
-    this.classList.add("black")
+    this.classList.add("black");
 }
