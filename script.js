92fd7ab5bc5de77f58ece75764604a9fc796b9bf
Chaostastic <roodey72mc@gmail.com>
2022-11-04 13:27:44 +0200

    Prompt for grid size

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const gridSize = 16;
 const grid = document.querySelector(".grid");
 
-function generateGrid() {
+function generateGrid(gridSize) {
     for (let rowIndex = 1; rowIndex <= gridSize; rowIndex++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
@@ -9,12 +8,19 @@ function generateGrid() {
         for (let pixelIndex = 1; pixelIndex <= gridSize; ++pixelIndex) {
             const pixel = document.createElement("div");
             pixel.classList.add("pixel");
+            pixel.style.height = 512 / gridSize + "px"
+            pixel.style.width = 512 / gridSize + "px"
             gridRow.appendChild(pixel);
         }
     }
 }
 
-generateGrid();
+const gridSize = prompt("Choose grid size");
+if (gridSize > 0 && gridSize <= 100) {
+    generateGrid(gridSize);
+} else {
+    alert("Invalid input!");
+}
 
 const pixels = document.querySelectorAll(".pixel");
 pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel));
