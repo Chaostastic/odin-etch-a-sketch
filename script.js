e2521773eabc983052bc0b6a1b3317f14fab3316
Chaostastic <roodey72mc@gmail.com>
2022-11-04 14:08:44 +0200

    Add resolution and clear canvas button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const grid = document.querySelector(".grid");
-
 function generateGrid(gridSize) {
+    const grid = document.querySelector(".grid");
+    removeAllChildNodes(grid);
+    resolutionButton.textContent = "Resolution: " + gridSize + "x" + gridSize;
     for (let rowIndex = 1; rowIndex <= gridSize; rowIndex++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
@@ -8,23 +9,44 @@ function generateGrid(gridSize) {
         for (let pixelIndex = 1; pixelIndex <= gridSize; ++pixelIndex) {
             const pixel = document.createElement("div");
             pixel.classList.add("pixel");
-            pixel.style.height = 512 / gridSize + "px"
-            pixel.style.width = 512 / gridSize + "px"
+            pixel.style.height = 512 / gridSize + "px";
+            pixel.style.width = 512 / gridSize + "px";
             gridRow.appendChild(pixel);
-        }
-    }
-}
+        };
+    };
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel));
+};
 
-const gridSize = prompt("Choose grid size");
-if (gridSize > 0 && gridSize <= 100) {
-    generateGrid(gridSize);
-} else {
-    alert("Invalid input!");
-}
+function changeSize() {
+    const gridSize = prompt("Choose grid size");
+    if (gridSize > 0 && gridSize <= 100) {
+        generateGrid(gridSize);
+    } else {
+        alert("Invalid input!");
+    };
+};
 
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel));
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
+
+function clearCanvas() {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => pixel.classList.remove("black"));
+};
 
 function colorPixel() {
     this.classList.add("black");
-}
+};
+
+const resolutionButton = document.querySelector(".resolution");
+resolutionButton.addEventListener("click", changeSize);
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", clearCanvas);
+
+generateGrid(16);
+
