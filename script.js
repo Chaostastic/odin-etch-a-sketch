7b62d8cb67e64cace181a18674695c15f5343ded
Chaostastic <roodey72mc@gmail.com>
2022-11-13 08:22:24 +0200

    Remove semicolons

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
-let selectedColor = "black";
+let selectedColor = "black"
 
 function generateGrid(gridSize) {
-    const grid = document.querySelector(".grid");
-    const pixelCount = Math.pow(gridSize, 2);
-    removeAllChildNodes(grid);
+    const grid = document.querySelector(".grid")
+    const pixelCount = Math.pow(gridSize, 2)
+    removeAllChildNodes(grid)
     for (let i = 1; i <= pixelCount; i++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.style.height = 512 / gridSize + "px";
-        pixel.style.width = 512 / gridSize + "px";
-        grid.appendChild(pixel);
-    };
-    const pixels = document.querySelectorAll(".pixel");
-    pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel));
-};
+        const pixel = document.createElement("div")
+        pixel.classList.add("pixel")
+        pixel.style.height = 512 / gridSize + "px"
+        pixel.style.width = 512 / gridSize + "px"
+        grid.appendChild(pixel)
+    }
+    const pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(pixel => pixel.addEventListener("mouseenter", colorPixel))
+}
 
 function changeSize(event) {
     const gridSize = event.target.value
     if (gridSize > 0 && gridSize <= 100) {
-        generateGrid(gridSize);
+        generateGrid(gridSize)
     } else {
-        alert("Invalid input!");
-    };
-};
+        alert("Invalid input!")
+    }
+}
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    };
-};
+        parent.removeChild(parent.firstChild)
+    }
+}
 
 function clearCanvas() {
-    const pixels = document.querySelectorAll(".pixel");
-    pixels.forEach(pixel => pixel.style.background = "white");
-};
+    const pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(pixel => pixel.style.background = "white")
+}
 
 function colorPixel() {
-    this.style.background = selectedColor;
-};
+    this.style.background = selectedColor
+}
 
 function changeColor(color) {
-    selectedColor = color.dataset.color;
-    colorSelectors.forEach(color => color.classList.remove("selected"));
-    color.classList.add("selected");
+    selectedColor = color.dataset.color
+    colorSelectors.forEach(color => color.classList.remove("selected"))
+    color.classList.add("selected")
 }
 
 function initColor(color) {
-    color.style.background = color.dataset.color;
-    color.addEventListener("click", event => changeColor(event.target));
+    color.style.background = color.dataset.color
+    color.addEventListener("click", event => changeColor(event.target))
 }
 
 function addColor(event) {
-    const customColors = document.querySelector(".custom.colors-container");
-    const color = document.createElement("div");
-    color.classList.add("color");
-    color.dataset.color = event.target.value;
-    initColor(color);
-    customColors.appendChild(color);
-    colorSelectors = document.querySelectorAll(".color");
-    changeColor(color);
+    const customColors = document.querySelector(".custom.colors-container")
+    const color = document.createElement("div")
+    color.classList.add("color")
+    color.dataset.color = event.target.value
+    initColor(color)
+    customColors.appendChild(color)
+    colorSelectors = document.querySelectorAll(".color")
+    changeColor(color)
 }
 
 function displayResolution(event) {
     const input = event.target.value
     console.log(input)
-    document.querySelector(".resolution").textContent = "Resolution: " + input + "x" + input;
+    document.querySelector(".resolution").textContent = "Resolution: " + input + "x" + input
 }
 
-const resolutionInput = document.querySelector("input[type='range']");
-resolutionInput.addEventListener("change", changeSize);
-resolutionInput.addEventListener("input", displayResolution);
+const resolutionInput = document.querySelector("input[type='range']")
+resolutionInput.addEventListener("change", changeSize)
+resolutionInput.addEventListener("input", displayResolution)
 
-const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", clearCanvas);
+const clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", clearCanvas)
 
-let colorSelectors = document.querySelectorAll(".color");
-colorSelectors.forEach(initColor);
+let colorSelectors = document.querySelectorAll(".color")
+colorSelectors.forEach(initColor)
 
 const customColorInput = document.querySelector("input[type='color']")
-customColorInput.addEventListener("change", addColor);
+customColorInput.addEventListener("change", addColor)
 
-generateGrid(16);
+generateGrid(16)
