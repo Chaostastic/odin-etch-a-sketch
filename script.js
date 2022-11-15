1657aacf2f9ff05636b45e1cb2bef90de8b7c468
Chaostastic <roodey72mc@gmail.com>
2022-11-15 09:44:11 +0200

    Add rainbow color option

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let selectedColorValue = "black"
 let pixelList
 let mouseDown
 let selectedColor = document.querySelector(".color.selected")
+const rainbowButton = document.querySelector(".rainbow")
 const clearButton = document.querySelector(".clear")
 const colorSelectorList = document.querySelectorAll(".color")
 const resolutionInput = document.querySelector("input[type='range']")
 const customColorInput = document.querySelector("input[type='color']")
+
+let getColor = () => "black"
 
 function generateGrid(gridSize) {
     const grid = document.querySelector(".grid")
@@ -35,13 +37,13 @@ function clearCanvas() {
 
 function colorPixel(event) {
     if (event.type === "mouseover" && !mouseDown) return
-    event.target.style.background = selectedColorValue
+    event.target.style.background = getColor()
 }
 
 function selectColor(color) {
     selectedColor.classList.remove("selected")
     selectedColor = color
-    selectedColorValue = selectedColor.dataset.color
+    getColor = () => selectedColor.dataset.color
     selectedColor.classList.add("selected")
 }
 
@@ -68,12 +70,20 @@ function displayResolution(event) {
     document.querySelector(".resolution").textContent = "Resolution: " + input + "x" + input
 }
 
+function rainbowColor() {
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+    return `rgb(${red} ${green} ${blue})`
+}
+
 document.addEventListener("mousedown", () => mouseDown = true);
 document.addEventListener("mouseup", () => mouseDown = false);
 resolutionInput.addEventListener("change", event => generateGrid(event.target.value))
 resolutionInput.addEventListener("input", displayResolution)
 customColorInput.addEventListener("change", addColor)
 clearButton.addEventListener("click", clearCanvas)
+rainbowButton.addEventListener("click", () => getColor = rainbowColor)
 colorSelectorList.forEach(initColor)
 
 generateGrid(16)
